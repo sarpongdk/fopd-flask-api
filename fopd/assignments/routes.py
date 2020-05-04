2df52d9f9cefb70dffda52a7c5b34aebf03b1ac0
sarpongdk <sarpong.david2@gmail.com>
2020-05-03 20:16:59 -0500

    minor fix

--- fopd/assignments/routes.py
+++ fopd/assignments/routes.py
@@ -159,7 +159,8 @@ def create_assignment(teacher_id):
         description = assignment_info['description'],
         type = assignment_info['type'],
         due_date = str(assignment_info.get('due_date', datetime.date.today())),
-        public_id = str(uuid.uuid4())
+        public_id = str(uuid.uuid4()),
+        submitted = assignment_info.get('submitted', None)
     )  
 
     assignment.teacher = teacher
